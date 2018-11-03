dd4e747b3a5ddcedaa66e78a70524bbd8370edfb
SatoShun <shun.sato1@gmail.com>
2018-11-03 08:35:44 +0000

    add RecyclerView mehtod

--- espresso-contrib/src/main/java/com/github/satoshun/espresso/contrib/RecyclerViewActions.kt
+++ espresso-contrib/src/main/java/com/github/satoshun/espresso/contrib/RecyclerViewActions.kt
@@ -4,7 +4,12 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.ViewAction
 import androidx.test.espresso.ViewInteraction
 import androidx.test.espresso.contrib.RecyclerViewActions
+import org.hamcrest.Matcher
 
 fun ViewInteraction.actionOnItemAtPosition(position: Int, action: ViewAction): ViewInteraction {
   return perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(position, action))
+}
+
+fun ViewInteraction.scrollToHolder(matcher: Matcher<out RecyclerView.ViewHolder>): ViewInteraction {
+  return perform(RecyclerViewActions.scrollToHolder(matcher))
 }
