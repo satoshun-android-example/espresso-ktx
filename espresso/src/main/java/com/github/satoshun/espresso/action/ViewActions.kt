38db781061f57ac0d5a25d0ad788b1f89ffdb82b
SatoShun <shun.sato1@gmail.com>
2018-11-18 09:13:15 +0000

    add some apis

--- espresso/src/main/java/com/github/satoshun/espresso/action/ViewActions.kt
+++ espresso/src/main/java/com/github/satoshun/espresso/action/ViewActions.kt
@@ -1,10 +1,35 @@
 package com.github.satoshun.espresso.action
 
+import androidx.test.espresso.ViewAction
 import androidx.test.espresso.ViewInteraction
 import androidx.test.espresso.action.ViewActions
 
+fun ViewInteraction.actionWithAssertions(viewAction: ViewAction): ViewInteraction {
+  return perform(ViewActions.actionWithAssertions(viewAction))
+}
+
+fun ViewInteraction.clearText(): ViewInteraction {
+  return perform(ViewActions.clearText())
+}
+
 fun ViewInteraction.click(): ViewInteraction {
   return perform(ViewActions.click())
+}
+
+fun ViewInteraction.swipeLeft(): ViewInteraction {
+  return perform(ViewActions.swipeLeft())
+}
+
+fun ViewInteraction.swipeRight(): ViewInteraction {
+  return perform(ViewActions.swipeRight())
+}
+
+fun ViewInteraction.swipeDown(): ViewInteraction {
+  return perform(ViewActions.swipeDown())
+}
+
+fun ViewInteraction.swipeUp(): ViewInteraction {
+  return perform(ViewActions.swipeUp())
 }
 
 fun ViewInteraction.typeText(stringToBeTyped: String): ViewInteraction {
--- espresso/src/test/java/com/github/satoshun/espresso/action/ViewActionsTest.kt
+++ espresso/src/test/java/com/github/satoshun/espresso/action/ViewActionsTest.kt
@@ -6,6 +6,7 @@ import android.widget.EditText
 import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Ignore
@@ -15,12 +16,72 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 internal class ViewActionsTest {
   @Test
+  fun actionWithAssertions() {
+    val scenario = ActivityScenario.launch(Activity::class.java)
+    scenario.moveToState(Lifecycle.State.RESUMED)
+    Espresso
+      .onView(ViewMatchers.withId(android.R.id.content))
+      .actionWithAssertions(ViewActions.click())
+  }
+
+  @Test
+  fun clearText() {
+    val id = 100
+    val scenario = ActivityScenario.launch(Activity::class.java)
+    scenario.moveToState(Lifecycle.State.RESUMED)
+    scenario.onActivity {
+      val edit = EditText(it)
+      edit.id = id
+      it.findViewById<ViewGroup>(android.R.id.content).addView(edit)
+    }
+    Espresso
+      .onView(ViewMatchers.withId(id))
+      .clearText()
+  }
+
+  @Test
   fun click() {
     val scenario = ActivityScenario.launch(Activity::class.java)
     scenario.moveToState(Lifecycle.State.RESUMED)
     Espresso
       .onView(ViewMatchers.withId(android.R.id.content))
       .click()
+  }
+
+  @Test
+  fun swipeLeft() {
+    val scenario = ActivityScenario.launch(Activity::class.java)
+    scenario.moveToState(Lifecycle.State.RESUMED)
+    Espresso
+      .onView(ViewMatchers.withId(android.R.id.content))
+      .swipeLeft()
+  }
+
+  @Test
+  fun swipeRight() {
+    val scenario = ActivityScenario.launch(Activity::class.java)
+    scenario.moveToState(Lifecycle.State.RESUMED)
+    Espresso
+      .onView(ViewMatchers.withId(android.R.id.content))
+      .swipeRight()
+  }
+
+  @Test
+  fun swipeDown() {
+    val scenario = ActivityScenario.launch(Activity::class.java)
+    scenario.moveToState(Lifecycle.State.RESUMED)
+    Espresso
+      .onView(ViewMatchers.withId(android.R.id.content))
+      .swipeDown()
+  }
+
+  @Test
+  fun swipeUp() {
+    val scenario = ActivityScenario.launch(Activity::class.java)
+    scenario.moveToState(Lifecycle.State.RESUMED)
+    Espresso
+      .onView(ViewMatchers.withId(android.R.id.content))
+      .swipeUp()
   }
 
   @Test
